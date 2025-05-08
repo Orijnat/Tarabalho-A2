2aab1620c6d2e89e73840e51caba7da19b6af8ed
Orijnat <cledivanleao@gmail.com>
2025-05-08 13:19:05 -0300

    ibubblesort na ordem crescente adicionado

--- main.c
+++ main.c
@@ -4,9 +4,28 @@
 //criacao do vetor
 
 #define VALOR  10
-int i, numeros[VALOR];
+int i, numeros[VALOR], contador, aux;
 
+
+//funçao bubblesort para organizar a lista de forma crescente
 void bubble_sort_crescente() {
+    for (contador=1; contador<VALOR; contador++) {
+        for (i=0; i < VALOR -1; i++) {
+            if (numeros[i] > numeros[i+1]) {
+                aux = numeros[i];
+                numeros[i] = numeros[i+1];
+                numeros[i+1] = aux;
+
+            }
+        }
+    }
+
+    printf("vetor organizado de forma crescente com bubblesort\n");
+    for(i=0; i < VALOR; i++){
+
+        printf("%d\n", numeros[i]);
+    }
+
 
 
 }
@@ -17,12 +36,28 @@ void bubble_sort_crescente() {
 
 int main()
 {
+    printf("ordem inicial dos valores\n");
     for(i=0; i < VALOR; i++){
 
-        numeros[i]= rand()%10000;
+        numeros[i]= rand()%10;
 
         printf("%d\n", numeros[i]);
     }
+
+    for (contador=1; contador<VALOR; contador++) {
+        for (i=0; i < VALOR -1; i++) {
+            if (numeros[i] > numeros[i+1]) {
+                aux = numeros[i];
+                numeros[i] = numeros[i+1];
+                numeros[i+1] = aux;
+
+            }
+        }
+    }
+
+
+bubble_sort_crescente(numeros);
+
 
     return 0;
 }
