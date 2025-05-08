10e2c66fdd4b6f49530a6a4ea3cd1841ac28988e
Orijnat <cledivanleao@gmail.com>
2025-05-08 12:53:54 -0300

    inicio do projeto

---
+++ main.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+//criacao do vetor
+
+#define VALOR  10
+int i, vetor[VALOR];
+
+void bubble_sort_crescente() {
+
+
+}
+
+
+
+
+
+int main()
+{
+    for(i=0; i < VALOR; i++){
+
+        vetor[i]= rand()%10000;
+
+        printf("%d\n", vetor[i]);
+    }
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by TECHCELL on 08/05/2025.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif //MAIN_H
